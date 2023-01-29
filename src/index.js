dae4fee552deabfb93d5579cc41d948cf06dd285
Alex-Sorokolit <sorokolital@gmail.com>
2023-01-29 14:31:38 +0200

    add persistor

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Redux
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 // Component
 import { App } from 'components/App';
 import './index.css';
+// Persist (lib for localStorage)
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,41 @@
 // файл створення стор Redux
 import { configureStore } from '@reduxjs/toolkit';
 
+// Persist
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //localStorage
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 // Slices
 import { contactsReducer } from './contacts/contacts-slice';
 import { filterReducer } from './filter/filter-slice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
+    // contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
