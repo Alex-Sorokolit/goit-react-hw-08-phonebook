3d92204832cb1ac3654e35f5dcffcfc982559260
Alex-Sorokolit <104554703+Alex-Sorokolit@users.noreply.github.com>
2023-02-01 14:50:20 +0200

    yup

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,33 +2,45 @@ import { useDispatch } from 'react-redux';
 import css from './RegisterForm.module.css';
 import { register } from 'redux/auth/auth-operations';
 import { SubmitBtn } from 'components/SubmitBtn/SubmitBtn';
-import { Formik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-export const RegisterForm = (values, actions) => {
+export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const onSubmit = event => {
-    event.preventDefault();
-    const form = event.currentTarget;
-
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
   const formikInitialValue = {
-    name: '123',
-    email: 'qwe',
-    password: 'asd',
+    name: '',
+    email: '',
+    password: '',
   };
+
+  const onSubmit = values => {
+    console.log(values);
+    dispatch(register(values));
+    // event.preventDefault();
+    // const form = event.currentTarget;
+    // dispatch(
+    //   register({
+    //     name: form.elements.name.value,
+    //     email: form.elements.email.value,
+    //     password: form.elements.password.value,
+    //   })
+    // );
+    // form.reset();
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string()
+      .min(7, 'Password must contain at least 7 characters')
+      .required(),
+  });
+
   return (
     <Formik initialValues={formikInitialValue} onSubmit={onSubmit}>
       {({ handleSubmit }) => (
-        <form
+        <Form
           className={css.form}
           onSubmit={handleSubmit}
           name="register_form"
@@ -40,28 +52,28 @@ export const RegisterForm = (values, actions) => {
               Username must consist of two words, first name and last name,
               separated by a space.
             </p>
-            <input
+            <Field
               className={css.input}
               type="text"
               name="name"
               required
               autoFocus
               placeholder=" "
-              pattern="^[a-zA-Z]+\s[a-zA-Z]+$"
-              title="Example: John Wick"
+              // pattern="^[a-zA-Z]+\s[a-zA-Z]+$"
+              // title="Example: John Wick"
             />
           </label>
           <label className={css.label}>
             Email
             <p className={css.description}>Enter E-mail address</p>
-            <input
+            <Field
               className={css.input}
               type="email"
               name="email"
               required
               placeholder=" "
-              pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-              title="Example: wick@mail.com"
+              // pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              // title="Example: wick@mail.com"
             />
           </label>
           <label className={css.label}>
@@ -70,20 +82,20 @@ export const RegisterForm = (values, actions) => {
               Must have at least 7 characters (1 character uppercase , 1
               lowercase and 1 number)
             </p>
-            <input
+            <Field
               className={css.input}
               type="password"
               name="password"
               required
-              minLength="7"
-              maxLength="12"
+              // minLength="7"
+              // maxLength="12"
               placeholder=" "
-              pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
-              title="Example: Xn2isL0"
+              // pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
+              // title="Example: Xn2isL0"
             />
           </label>
           <SubmitBtn>Register</SubmitBtn>
-        </form>
+        </Form>
       )}
     </Formik>
   );
