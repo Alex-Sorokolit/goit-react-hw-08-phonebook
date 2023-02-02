769abd6f32b9c357a89b275035b5ed07f20f5c3f
Alex-Sorokolit <sorokolital@gmail.com>
2023-02-02 12:01:10 +0200

    bugfix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/contacts/contacts-operations';
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from 'redux/contacts/contacts-selectors';
+
 import css from './Contact.module.css';
 import { MdDelete } from 'react-icons/md';
 
 export const Contact = ({ contact }) => {
   const dispatch = useDispatch();
-
-  // const classNames = ['css.deleteBtn'];
-  // const loading = useSelector(selectIsLoading);
-  // if (loading) classNames.push('css.disabled');
 
   const handleDelete = () => dispatch(deleteContact(contact.id));
   return (
@@ -18,12 +13,7 @@ export const Contact = ({ contact }) => {
       <p className={css.name}>
         {contact.name}: {contact.number}
       </p>
-      <button
-        // className={classNames.join(' ')}
-        className={css.deleteBtn}
-        type="buttone"
-        onClick={handleDelete}
-      >
+      <button className={css.deleteBtn} type="buttone" onClick={handleDelete}>
         <MdDelete />
       </button>
     </div>
